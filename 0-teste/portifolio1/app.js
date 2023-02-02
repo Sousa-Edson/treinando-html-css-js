44758d1aa57c173849e573d83568b2811d42e8b4
Edson Sousa <edson3711.es@gmail.com>
2023-02-02 06:37:08 -0300

    update

--- 0-teste/portifolio1/app.js
+++
@@ -1,8 +0,0 @@
-const btnMobile = document.getElementById('btn-mobile');
-const btnNav = document.getElementById('nav');
-
-function toggleMenu(event) {
-    nav.classList.toggle('active');
-}
-btnNav.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
